eb4b3ef3b74b9b7e95a787e15a93c7b7ff77cd59
David Ayeke <ayekedavid@yahoo.com>
2018-06-19 12:54:40 -0500

    [General] Ommiting the id field from users as username is the main index

--- models/user.go
+++ models/user.go
@@ -2,7 +2,7 @@ package models
 
 // User is a wrapper around a list of contacts.
 type User struct {
-	UserID   string `json:"id"`
+	UserID   string `json:"id,omitempty"`
 	Username string `json:"username"`
 	Password string `json:"password,omitempty"`
 	Contacts []Contact
